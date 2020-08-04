70fe290c617fb95d8060b873d7415a486989441d
Abijah Kabiro <abijahkabiro@gmail.com>
2020-08-04 14:56:03 +0300

    add a function that will calculate the total price charged

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,10 @@ function getTopping() {
     var selectedTopping= document.getElementById("toppings").value;
     return parseInt(selectedTopping);
 }
-
+function calctotalPrice(e) {
+    event.preventDefault();
+    var totalPrice = (getSizeCost() + getCrustCost() + getTopping()) * (getNumber());
+    
+        console.log(totalPrice);
+    alert("Your order of " + getNumber() + " pizzas has been processed.Your should pay " + totalPrice +"." )
+}
