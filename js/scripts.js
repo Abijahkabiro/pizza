197ee1fab4050cdc8ec170b62f366c4efd721eb2
Abijah Kabiro <abijahkabiro@gmail.com>
2020-08-04 15:04:39 +0300

    writing the user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,15 @@ function calctotalPrice(e) {
         console.log(totalPrice);
     alert("Your order of " + getNumber() + " pizzas has been processed.Your should pay " + totalPrice +"." )
 }
+
+
+$(document).ready(function () {
+    $("#delivery").submit(function () {
+
+        var name = $("input#name").val();
+        var number = $("input#number").val();
+        var location = $("input#location").val();
+
+        alert("Hello " + name + ". Your order has been successfuly received and will be delivered to " +  location + ".The delivery cost is ksh 200/= Thank you for chosing us.");
+    });  
+});
