826e8de4e85821884421ed1bcd4429d3401e5f70
Abijah Kabiro <abijahkabiro@gmail.com>
2020-08-04 14:42:35 +0300

    add an onions function to get price of onions

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,11 @@ function getNumber() {
     var selectedNumber = document.getElementById("number").value;
     return parseInt(selectedNumber);
 }
+function onions() {
+    var onion = 0;
+    var addOnion = document.getElementById("topping");
+    if (addOnion.checked === true) {
+        onion= 70;
+    }
+    return parseInt(onion);
+}
