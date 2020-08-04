1c9f69f2179c9873df467ba2d04bb6b555809432
Abijah Kabiro <abijahkabiro@gmail.com>
2020-08-04 14:41:08 +0300

    add tthe number of pizza function

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,7 @@ function getCrustCost() {
     var selectedCrust = document.getElementById("crust").value;
     return parseInt(selectedCrust);
 }
+function getNumber() {
+    var selectedNumber = document.getElementById("number").value;
+    return parseInt(selectedNumber);
+}
